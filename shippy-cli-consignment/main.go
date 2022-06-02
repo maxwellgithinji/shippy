5c4b24964398d9ee6f22d608baca947e772c1856
maxwellgithinji <maxwellgithinji@gmail.com>
2022-06-02 22:09:53 +0300

    chore: add go micro - this will help with some aspects we will need for the microservices to work - service discovery is one of the concepts we will need to implement in the microservices architecture - the version of the cli tool (v4) matches that of the go micro versions - an alternative of go micro is go-toolkit
    Signed-off-by: maxwellgithinji <maxwellgithinji@gmail.com>

--- shippy-cli-consignment/main.go
+++ shippy-cli-consignment/main.go
@@ -9,7 +9,7 @@ import (
 	"context"
 
 	pb "github.com/maxwellgithinji/shippy/shippy-service-consignment/proto/consignment"
-	"google.golang.org/grpc"
+	micro "go-micro.dev/v4"
 )
 
 const (
@@ -28,13 +28,11 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+
+	service := micro.NewService(micro.Name("shippy.consignment.cli"))
+	service.Init()
+
+	client := pb.NewShippingService("shippy.consignment.service", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
